730095834605d4fcf0fd2312284296be53437172
Matthew Ogram <matthew_ogram@intuit.com>
2014-07-15 10:43:34 -0700

    WIP

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,5 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-////= require jquery
-////= require jquery_ujs
-////= require jquery.responsiveiframe
-//= require embed
-//= require app
-//= require_tree .
+//= require embed_v0
+//= require app_v0
