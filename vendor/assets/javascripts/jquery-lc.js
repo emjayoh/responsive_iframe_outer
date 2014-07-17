63f56e7727a736b7df63a7174df3bcbf1b5257bc
Matthew Ogram <matthew_ogram@intuit.com>
2014-07-17 16:13:09 -0700

    Testing wraps

--- vendor/assets/javascripts/jquery-lc.js
+++ vendor/assets/javascripts/jquery-lc.js
@@ -1,3 +1,8 @@
-define(['jquery'], function (jq) {
+(function (require, define) {
+  console.log(define);
+  define(['jquery'], function (jq) {
+    console.log(jq);
     return jq.noConflict( true );
-});
+  });
+}(myGlobalRequire[0], myGlobalRequire[1]));
+
